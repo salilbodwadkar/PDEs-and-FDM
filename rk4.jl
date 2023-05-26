aeaa000e41ec1b7b18017f24106e798e6ba01f56
salilbodwadkar <134647164+salilbodwadkar@users.noreply.github.com>
2023-05-26 15:46:58 -0700

    Update rk4.jl

--- rk4.jl
+++ rk4.jl
@@ -1,14 +1,15 @@
-function rungekutta4(f, y0, t)
-    n = length(t)
-    y = zeros((n, length(y0)))
-    y[1,:] = y0
-    for i in 1:n-1
-        h = t[i+1] - t[i]
-        k1 = f(y[i,:], t[i])
-        k2 = f(y[i,:] + k1 * h/2, t[i] + h/2)
-        k3 = f(y[i,:] + k2 * h/2, t[i] + h/2)
-        k4 = f(y[i,:] + k3 * h, t[i] + h)
-        y[i+1,:] = y[i,:] + (h/6) * (k1 + 2*k2 + 2*k3 + k4)
+function rk4(f, a, b, alpha, N)
+    # Solve ODE y'(t) = f(t, y(t)) using Runge-Kutta 4.
+    h = (b - a) / N
+    t = collect(a:h:b)
+    w = zeros(N+1, length(alpha))
+    w[1, :] = alpha
+    for i in 1:N
+        k1 = h * f(t[i], w[i, :])
+        k2 = h * f(t[i] + h/2, w[i, :] + k1/2)
+        k3 = h * f(t[i] + h/2, w[i, :] + k2/2)
+        k4 = h * f(t[i] + h, w[i, :] + k3)
+        w[i+1, :] = w[i, :] + (k1 + 2*k2 + 2*k3 + k4) / 6
     end
-    return y
+    return t, w
 end
