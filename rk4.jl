8fb9af1a0dbc7b09bca0f8753be6165e73201d18
salilbodwadkar <134647164+salilbodwadkar@users.noreply.github.com>
2023-05-26 15:59:08 -0700

    Update rk4.jl

--- rk4.jl
+++ rk4.jl
@@ -2,8 +2,8 @@ function rk4(f, a, b, u0, N)
     # Solve ODE y'(t) = f(t, y(t)) using Runge-Kutta 4.
     h = (b - a) / N
     t = collect(a:h:b)
-    w = zeros(N+1, length(alpha))
-    w[1, :] = alpha
+    w = zeros(N+1, length(u0))
+    w[1, :] = u0
     for i in 1:N
         k1 = h * f(t[i], w[i, :])
         k2 = h * f(t[i] + h/2, w[i, :] + k1/2)
