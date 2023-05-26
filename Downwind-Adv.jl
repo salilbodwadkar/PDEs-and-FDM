b525ebc333318c7e8d8b49e801ceddfadcbac682
salilbodwadkar <134647164+salilbodwadkar@users.noreply.github.com>
2023-05-26 15:36:06 -0700

    Create Downwind-Adv.jl

---
+++ Downwind-Adv.jl
@@ -0,0 +1,13 @@
+function downwind_scheme(u0, c, dx, dt, t_final)
+    n = length(u0)
+
+    nt = Int(t_final / dt)
+  
+    u = zeros(n, nt+1)
+    u[:, 1] = u0
+
+    for i in 1:nt
+        u[:, i+1] = u[:, i] - c * dt / dx * (u[:, i] - u[mod1.(1:n-1, n), i])
+    end
+    return u
+end
