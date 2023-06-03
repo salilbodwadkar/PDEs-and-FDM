a2c618906cf47ea4adbec18827a8c6251ba39ca3
Salil Bodwadkar <134647164+salilbodwadkar@users.noreply.github.com>
2023-06-03 13:15:20 -0700

    Update Lax-Fried-Non-Lin.jl

--- Lax-Fried-Non-Lin.jl
+++ Lax-Fried-Non-Lin.jl
@@ -1,15 +1,30 @@
-function Lax_Fried_NL(u0, dx, dt, t_final, f)
-   #u0 is initial condition matrix, c is velocity, dx and dt are space and time steps repsectively, f is flux function
+function Lax_Fried_NL(u0, c, L, N, dt, t_final)
+    #u0 is initial traffic density, c is advection velocity, L is road legnth, N is # of grid points, dt is time step
 
-    n = length(u0)
-  
-    nt = Int(t_final / dt)
+   using Plots
+   
+   dx = L/N
+   x = range(0, L, length=N+1)
 
-    u = zeros(n, nt+1)
-    u[:, 1] = u0
+   # define flux function
+   function flux(u)
+       return u * (1 - u) * c
+   end
 
-    for i in 1:nt
-        u[:, i+1] = 0.5 * (u[mod1.(1:n-1, n), i] + u[mod1.(2:n, n), i]) - 0.5 * dt / dx * (f(u[mod1.(2:n, n), i]) - f(u[mod1.(1:n-1, n), i]))
-    end
-    return u
+   # lax-Friedrichs scheme for non-linear advection
+   u = copy(u0)
+   plot(x, u, xlim=(0, L), ylim=(0, 2), xlabel="Position", ylabel="Density", title="Traffic Density at Time t = 0")
+
+   for t in 0:dt:t_final
+       u_next = zeros(N+1)
+       for i in 2:N
+           flux_val = flux(u[i])
+           u_next[i] = u[i] - (dt/dx) * (flux_val - flux(u[i-1]))
+       end
+       u = copy(u_next)
+
+       plot!(x, u)
+       plot!(legend=false)
+       display(plot!)
+   end
 end
