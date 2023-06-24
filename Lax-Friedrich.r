a3a6bc2961343f5257e037f76a67b4f70bd28492
Salil Bodwadkar <134647164+salilbodwadkar@users.noreply.github.com>
2023-06-23 17:05:20 -0700

    Create Lax-Friedrich.r

---
+++ Lax-Friedrich.r
@@ -0,0 +1,24 @@
+library(ggplot2)
+
+Lax_Friedrich <- function(u0, c, L, N, dt, t_final) {
+  dx <- L / N
+  x <- seq(0, L, length.out = N + 1)
+  
+  # Lax-Friedrichs scheme
+  u <- u0
+  p <- ggplot() + xlim(0, L) + ylim(0, 1.2) + xlab("Position") + ylab("Density") + ggtitle("Traffic Density at Time t = 0")
+  p <- p + geom_line(aes(x, u))
+  print(p)
+  
+  for (t in seq(0, t_final, by = dt)) {
+    u_next <- rep(0, N + 1)
+    for (i in 2:N) {
+      u_next[i] <- (u[i + 1] + u[i - 1]) / 2 - (c * dt / (2 * dx)) * (u[i + 1] - u[i - 1])
+    }
+    u <- u_next
+    
+    p <- p + geom_line(aes(x, u))
+    p <- p + theme(legend.position = "none")
+    print(p)
+  }
+}
