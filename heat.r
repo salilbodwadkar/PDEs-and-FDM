fd70b4533c53a0efac7019c00a3ed0779bd02cf0
Salil Bodwadkar <134647164+salilbodwadkar@users.noreply.github.com>
2023-06-23 17:09:08 -0700

    Create heat.r

---
+++ heat.r
@@ -0,0 +1,21 @@
+solve_heat_ftcs <- function(N, L, dt, t_final, alpha) {
+  N <- 100  # N is of spatial discretization points, L is legnth of domain, dt is time step size
+  
+  dx <- L / (N - 1)  # Spatial step size
+
+  x <- seq(0, L, length.out = N)  # Spatial grid
+
+  T0 <- sin(pi * x)  # Initial condition
+  T <- T0  # Solution array
+
+  r <- alpha * dt / dx^2  # Stability parameter
+
+  t <- 0
+  while (t < t_final) {
+    T_new <- T + r * (c(T[-1], 0) - 2 * T + c(0, T[-N]))  # FTCS update
+    T <- T_new
+    t <- t + dt
+  }
+
+  return(T)
+}
