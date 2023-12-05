84dabef14105548c50f25270b7ded5a27f64eec1
Salil Bodwadkar <134647164+salilbodwadkar@users.noreply.github.com>
2023-12-04 22:54:16 -0600

    Create Downwind-Adv.py

---
+++ Downwind-Adv.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def advection_downwind(f, a, X, T, t_final):
+   
+    # f = *function* for initial condition
+    # 1/X is space step size
+    # 1/T is time step size
+
+    Dx = 1/X
+    Dt = t_final/T
+
+    x = np.linspace(0, 1, X+1)  
+    t = np.linspace(0, t_final, T+1)
+    u = np.zeros((T, X))  
+
+    u[:,0] = f(x)
+
+    # Downwind scheme for advection equation
+    for i in range(1, T+1):
+        for j in range(X+1):
+            u[i, j] = u[i - 1, j] - (a * Dt / Dx) * (u[i - 1, j] - u[i - 1, j - 1])
+
+    return u
