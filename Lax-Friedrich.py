42771290efb860ef18b920a5607b5c5ced7b7fff
Salil Bodwadkar <134647164+salilbodwadkar@users.noreply.github.com>
2023-12-04 21:45:48 -0600

    Create Lax-Friedrich.py

---
+++ Lax-Friedrich.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def lax_friedrichs(f, X, T, a, X, T, t_final):
+   
+    # f = *function* for initial condition
+    # 1/X is space step size
+    # 1/T is time step size
+
+    Dx = 1/X
+    Dt = t_final/T
+
+    x = np.linspace(0, 1, X+1)  
+    t = np.linspace(0, t_final, T+1)
+    u = np.zeros((T, X))  
+
+    u[:, 0] = f(x)
+
+    # Lax-Friedrichs scheme
+    for i in range(1,T+1):
+        for j in range(X+1):
+            u[i, j] = 0.5 * (u[i - 1, j + 1] + u[i - 1, j - 1]) - (a * dt / (2 * dx)) * (u[i - 1, j + 1] - u[i - 1, j - 1])
+    return u
